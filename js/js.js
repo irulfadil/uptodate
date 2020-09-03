188490c6e6327eae9bc7966c19700111e11ff348
irulfadil <58598590+irulfadil@users.noreply.github.com>
2020-09-03 10:58:42 +0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,7 @@
+var s = '';
+for (var i =0; i<=6; i++) {
+    for (j = 0; j <=i; j++) {
+        s += '*';
+    }
+   s+='\n';
+} console.log(s);
---
+++ latihan1.php
@@ -0,0 +1,23 @@
+<?php 
+$mahasiswa = [
+    "nama" => "irulfadil",
+    "umur" => "33",
+    "alamat" => "kaliputu"
+];
+$data = json_encode($mahasiswa);
+echo $data;
+
+// $user = 'root';
+// $pass = '';
+//     $dbh = new PDO('mysql:host=127.0.0.1;dbname=test',$user,$pass);
+//     $db = $dbh->prepare('SELECT * FROM mahasiswa');
+//     $db->execute();
+//     // $mahasiswa = $db->fetchAll();
+//     $mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+//     $data = json_encode($mahasiswa);
+//     echo $data;
+ 
+    // $content = file_get_contents('coba.json');
+    // echo $result = json_decode($content,true);
+
+?>
